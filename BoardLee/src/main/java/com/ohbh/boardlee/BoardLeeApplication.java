f68a1f9d51edf23e4974f12e91fa0017c5d236f5
MHLEE12 <coolmh12@gmail.com>
2022-09-10 22:06:42 +0900

    new: created project files
    이 프로젝트는 게시판 프로젝트를 제작할 계획입니다.
    my mini project with jk.

---
+++ BoardLee/src/main/java/com/ohbh/boardlee/BoardLeeApplication.java
@@ -0,0 +1,13 @@
+package com.ohbh.boardlee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardLeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardLeeApplication.class, args);
+    }
+
+}
---
+++ BoardLee/src/test/java/com/ohbh/boardlee/BoardLeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohbh.boardlee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardLeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
